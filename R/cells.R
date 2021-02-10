c1162e207299f4148ba7c250fa083529b1bfa295
Marek Gierlinski <mgierlinski@dundee.ac.uk>
2021-02-10 11:34:14 +0000

    New figure

--- R/cells.R
+++ R/cells.R
@@ -1,3 +1,17 @@
+state_colour <- tribble(
+  ~state, ~colour,
+  "none", "grey70",
+  "black", "black",
+  "lightblue", "skyblue",
+  "darkblue", "mediumblue",
+  "brown", "brown",
+  "pink", "hotpink",
+  "red", "red"
+) %>% 
+  mutate(state = as_factor(state))
+
+
+
 # ds - data frame with two rows and columns x, y, z
 # finds distance between rows
 dist_xyz <- function(d) {
@@ -5,13 +19,19 @@ dist_xyz <- function(d) {
   sqrt(sum((r[2,] - r[1,])^2))
 }
 
+
+# ds - a subset of the main "dat" tibble, needs to contain 2-4 rows
+# and co-ordinate columns (x, y, z)
+# value - tibble with classification of the state
 parse_one_state <- function(ds, dist.lightblue = 0.3, dist.brown = 0.75, dist.pink = 0.3) {
   n_dots <- nrow(ds)
-  stopifnot(n_dots > 0 & n_dots <= 4)
   
   dst <- NULL
   
-  if(n_dots == 2) {
+  if(n_dots == 1) {
+    dst <- 0
+    state <- "none"
+  } else if(n_dots == 2) {
     dst <- dist_xyz(ds)
     state <- ifelse(dst > dist.lightblue, "lightblue", "black")
   } else if(n_dots == 3) {
@@ -48,5 +68,6 @@ parse_one_state <- function(ds, dist.lightblue = 0.3, dist.brown = 0.75, dist.pi
 parse_states <- function(d) {
   d %>% 
     group_split(cell, time_point) %>% 
-    map_dfr(~parse_one_state(.x))
+    map_dfr(~parse_one_state(.x)) %>% 
+    mutate(state = factor(state, levels=state_colour$state))
 }
--- R/plots.R
+++ R/plots.R
@@ -32,3 +32,19 @@ animate_cell <- function(d, cl) {
     transition_time(time_point) +
     labs(title = "{frame_time}")
 }
+
+plot_state_distance <- function(st) {
+  st %>% 
+    mutate(dist_max = pmax(dist_1, dist_2, na.rm=TRUE), time = time/60) %>% 
+  ggplot(aes(x=time, y=dist_1, fill=state, shape=factor(n_dot, levels=1:4))) +
+    geom_segment(aes(xend=time, yend=0, y=dist_max), colour="grey80") +
+    geom_point(size=3, colour="grey50") +
+    scale_fill_manual(values=state_colour$colour, drop=FALSE) +
+    scale_shape_manual(values=c(20, 21, 24, 23)) +
+    geom_point(aes(y=dist_2), shape=23, size=3, colour="grey50") +
+    theme_bw() +
+    theme(panel.grid = element_blank()) +
+    facet_wrap(~cell, ncol=1) +
+    scale_y_continuous(expand=expansion(mult=c(0, 0.05))) +
+    labs(x="Time (min)", y=expression(Distance~(mu * m)), shape="Num dots", colour="State")
+}
