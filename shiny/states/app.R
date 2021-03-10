2e3e260dfa3f84c2b9afba19ee1bb46a76fdbfcd
Marek Gierlinski <mgierlinski@dundee.ac.uk>
2021-03-10 14:05:19 +0000

    Tweaks

--- shiny/states/app.R
+++ shiny/states/app.R
@@ -1,4 +1,5 @@
 library(shiny)
+library(shinycssloaders)
 library(tidyverse)
 
 source("../setup.R")
@@ -28,18 +29,20 @@ ui <- fluidPage(
   sidebarLayout(
     sidebarPanel(
       actionButton("reload", "Reload data"),
-      p(),
-      selectInput("condition", "Conditions", choices=initial_conditions),
-      selectInput("cell", "Cell number", choices=initial_cells),
+      hr(),
       sliderInput("dist.lightblue", "Black/light blue limit", value=0.5, min=0, max=5, step=0.05),
       sliderInput("dist.brown", "Blue/brown limit", value=0.75, min=0, max=5, step=0.05),
       sliderInput("dist.pink", "Red/pink limit", value=0.4, min=0, max=5, step=0.05),
       sliderInput("black.length", "Black length", value=5, min=0, max=10, step=1),
       actionButton("submit", "Submit"),
+      hr(),
+      selectInput("condition", "Condition", choices=initial_conditions),
+      selectInput("cell", "Cell number", choices=initial_cells),
     ),
     
     mainPanel(
-      plotOutput("dist_state_plot", height="300px")
+      plotOutput("heatmap", height="100px") %>% withSpinner(color="#0dc5c1"),
+      plotOutput("dist_state_plot", height="300px") %>% withSpinner(color="#0dc5c1")
     )
   )
 )
@@ -47,17 +50,28 @@ ui <- fluidPage(
 
 server <- function(input, output, session) {
   
+  # We use isolate() to avoid dependency on input
+  # The plot will be updated only on "sumbit"
   params_from_input <- function() {
-    list(
+    isolate(list(
       dist.lightblue = input$dist.lightblue,
       dist.brown = input$dist.brown,
       dist.pink = input$dist.pink,
       black.length = input$black.length
-    )
+    ))
   }
   
   observeEvent(input$reload, {
-    reload_data(data.path, cell_sheets, cache.file)
+    withProgress(message = "Loading Excel files", {
+      metadata <- get_metadata(data.path)
+      incProgress(1/4)
+      dat <- read_cells(metadata, cell_sheets)
+      incProgress(1/4)
+      dat <- process_raw_data(dat, with_celldat=FALSE)
+      incProgress(1/4)
+      write_rds(dat, cache.file)
+      incProgress(1/4)
+    })
   })
   
   dat <- eventReactive(input$submit, {
@@ -69,11 +83,20 @@ server <- function(input, output, session) {
   })
   
   output$dist_state_plot <- renderPlot({
+    input$submit
     d <- dat()
     params <- params_from_input()
     d$parsed %>% 
       filter(condition == input$condition & cell == input$cell) %>% 
       pl_state_distance(params)
+  })
+  
+  output$heatmap <- renderPlot({
+    input$submit
+    d <- dat()
+    d$parsed %>% 
+      filter(condition == input$condition) %>% 
+      pl_state_map()
   })
   
 }
--- shiny/states/func.R
+++ shiny/states/func.R
@@ -24,7 +24,7 @@ pl_state_distance <- function(dp, params) {
   ds <- drop_na(d)
   ggplot(d, aes(x=time_nebd, y=dist_1, fill=state, shape=factor(n_dot, levels=1:4))) +
     theme_bw() +
-    theme(panel.grid = element_blank()) +
+    theme(panel.grid = element_blank(), legend.position = "none") +
     geom_hline(data=dd, aes(yintercept = limit, colour=state), linetype="dotted") +
     geom_segment(aes(xend=time_nebd, yend=0, y=dist_max), colour="grey80") +
     geom_point(size=3, colour="grey50") +
@@ -35,4 +35,18 @@ pl_state_distance <- function(dp, params) {
     geom_point(data =  ds, aes(y=dist_2), shape=23, size=3, colour="grey50") +
     scale_y_continuous(expand=expansion(mult=c(0, 0.05))) +
     labs(x="Time since nebd (min)", y=expression(Distance~(mu * m)), shape="Num dots", colour="State")
+}
+
+pl_state_map <- function(dp) {
+  dp %>% 
+    ggplot(aes(x=time_nebd, y=as_factor(cell) %>% fct_rev(), fill=state)) +
+    theme_bw() +
+    theme(
+      panel.grid = element_blank(),
+      legend.position = "none"
+      #axis.text.y = element_blank()
+    ) +
+    geom_tile() +
+    scale_fill_manual(values=state_colour$colour, drop=FALSE) +
+    labs(x="Time since nebd (min)", y=NULL, fill=NULL)
 }
