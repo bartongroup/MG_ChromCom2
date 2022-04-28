9feaa4874056b1210ae92a4ae21ff1a50a1a2913
Marek Gierlinski <mgierlinski@dundee.ac.uk>
2022-04-28 08:54:27 +0100

    rsync delete and compress added

--- copy_data_to_cluster.sh
+++ copy_data_to_cluster.sh
@@ -1,1 +1,1 @@
-rsync -rvm ./data/* cluster:/cluster/gjb_lab/mgierlinski/projects/chromcom2/data
+rsync -rvm --delete --compress ./data/* cluster:/cluster/gjb_lab/mgierlinski/projects/chromcom2/data
