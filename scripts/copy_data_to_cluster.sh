c4ed79b07cf44cb764465b87a63309cf7f619ca4
Marek Gierlinski <mgierlinski@dundee.ac.uk>
2026-07-02 11:14:53 +0100

    New data

--- scripts/copy_data_to_cluster.sh
+++ scripts/copy_data_to_cluster.sh
@@ -1,3 +1,3 @@
 #rsync -rvm --delete --compress ./data/* cluster:/cluster/gjb_lab/mgierlinski/projects/chromcom2/data
 rsync -rvm --delete --compress ./data/* shiny:/home/mgierlinski/ShinyApps/private/chromcom2/data
-
+rsync -rvm ./shiny/parsing_states/cache/* shiny:/home/mgierlinski/ShinyApps/private/chromcom2/parsing_states/cache
